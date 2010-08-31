a5e9d0e56ef495474cbe814edd11531b1fe43cc0
mdms-scott <mdms.scott@gmail.com>
2010-08-30 20:13:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rpress::Application.config.secret_token = '8a7d4d478e6782edab3d731581034edc600ce80e23cbae26a90758628e9fed015e7fbe55be8c3b2b68fcfd158bf43f9da1bcf921ffe530b02b3fab0bf9d09b3b'
